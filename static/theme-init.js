c8c0f5be598d9d51a09f886d9c7de8d85592892f
Shivang Shukla <shivangshukla8719@gmail.com>
2026-09-06 16:18:09 +0530

    Empty repository

--- static/theme-init.js
+++
@@ -1,7 +0,0 @@
-// Applied as an external, synchronously-loaded <head> script (not inline) so
-// it satisfies a strict `script-src 'self'` CSP while still running before
-// first paint to avoid a flash of the wrong theme.
-(function () {
-  var theme = localStorage.getItem("theme");
-  if (theme) document.documentElement.setAttribute("data-theme", theme);
-})();
